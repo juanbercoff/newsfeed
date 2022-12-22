d32832fc61bc7fa00ddfad15a20d96f3e1ffbe32
Juan <jmbercoff@gmail.com>
2022-12-22 20:09:11 -0300

    favicon

---
+++ apps/website/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Summas</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
