3c249b9097c724120e31df27fa03699c76929ca8
Juan <jmbercoff@gmail.com>
2022-12-23 12:16:47 -0300

    config

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
-const { i18n } = require('./next-i18next.config.js');
 const path = require('path');
 
 /**
