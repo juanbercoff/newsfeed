3dccff3e95864b09f7112858206a21947c7fc091
Juan <jmbercoff@gmail.com>
2022-12-23 21:06:08 -0300

    change nexti18n config

--- apps/website/next-i18next.config.js
+++ apps/website/next-i18next.config.js
@@ -5,5 +5,5 @@ module.exports = {
     defaultLocale: 'en',
     locales: ['en', 'es'],
   },
-  localePath: path.resolve('./apps/website/public/locales'),
+  localePath: path.resolve('./public/locales'),
 };
